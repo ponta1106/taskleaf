45b0d91d543187c8cc8ea4775f531eb19d8e48cd
Tabata Nobuya <qqkz2qf9k@kind.ocn.ne.jp>
2021-06-26 23:56:33 +0900

    20210626

---
+++ app/models/task.rb
@@ -0,0 +1,15 @@
+class Task < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 30 }, uniqueness: true
+  validate :validate_name_not_including_comma
+  
+  scope :recent, -> { order(created_at: :desc) }
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+  
+end
